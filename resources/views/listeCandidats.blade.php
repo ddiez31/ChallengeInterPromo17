7f3bc5cf071b82c03ad39822788c95928675ac8a
thibault <thibault.daboville@gmail.com>
2017-06-14 10:15:08 +0200

    listeCandidat ok coté front

--- resources/views/listeCandidats.blade.php
+++ resources/views/listeCandidats.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
+        <div class="col-xs-4" style="text-align: center; border: 1px solid black";>
+            Nom
+        </div>
+        <div class="col-xs-4" style="text-align: center; border: 1px solid black";>
+            Prénom
+        </div>
+        <div class="col-xs-4" style="text-align: center; border: 1px solid black";>
+            Localité
         </div>
     </div>
 </div>
