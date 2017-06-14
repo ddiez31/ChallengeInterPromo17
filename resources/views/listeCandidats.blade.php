474893c580554ac8687fe328c2d296bc197e0a65
thibault <thibault.daboville@gmail.com>
2017-06-14 10:45:33 +0200

    ajout du foreach qui va récupérer les données de la BDD et les afficher dans le tableau

--- resources/views/listeCandidats.blade.php
+++ resources/views/listeCandidats.blade.php
@@ -13,5 +13,18 @@
             Localité
         </div>
     </div>
+    <div class="row">
+        @foreach ($users as $user)
+        <div class="col-xs-4" style="text-align: center; border: 1px solid black";>
+            <p>{{$user-> nom}}</p>
+        </div>
+        <div class="col-xs-4" style="text-align: center; border: 1px solid black";>
+            <p>{{$user-> prenom}}</p>
+        </div>
+        <div class="col-xs-4" style="text-align: center; border: 1px solid black";>
+            <p>{{$user-> localite}}</p>
+        </div>
+        @endforeach
+    </div>
 </div>
 @endsection
